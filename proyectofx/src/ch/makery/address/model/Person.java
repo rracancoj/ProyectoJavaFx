fdfeda8b5088de4b2bcdef1a04f1f9e1ca56fd96
roberto <mail@mail.com>
2017-11-03 11:38:09 +0100

    quinta parte del proyecto fx

--- proyectofx/src/ch/makery/address/model/Person.java
+++ proyectofx/src/ch/makery/address/model/Person.java
@@ -9,6 +9,10 @@ import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import ch.makery.address.util.LocalDateAdapter;
+
 /**
  * Model class for a Person.
  *
@@ -107,6 +111,7 @@ public class Person {
 		return city;
 	}
 
+	@XmlJavaTypeAdapter(LocalDateAdapter.class)
 	public LocalDate getBirthday() {
 		return birthday.get();
 	}
